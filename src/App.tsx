efda92a55487344d42e74c74ceb8e5a7b7731b09
Inkyu Kim <inkyu0103@gmail.com>
2021-09-19 01:25:52 +0900

    chore:firebase 환경설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const App = () => {
-  return <h1>애플워치 받고 싶어용!</h1>;
+  return <h1>애플워치SE 받고 싶어용!</h1>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import firebase from "./lib/firebase";
+
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(<App />, rootElement);
